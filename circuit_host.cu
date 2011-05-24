e81eaed9559efdf48f1247e7485a16a4b18c7949
Ting <tingyu1@acfs.(none)>
2011-05-24 12:01:59 -0500

    only store b in shared memory

--- circuit_host.cu
+++ circuit_host.cu
@@ -18,7 +18,7 @@ cudaChannelFormatDesc channelDesc =
 		cudaCreateChannelDesc(32, 0, 0, 0, cudaChannelFormatKindFloat);
 
 // kernel function, doing forward and backward substitution
-// data stored in L_d: nz, col, row, and x
+// data stored in L_d: row, col, and val
 // data stored in b_x_d: b and x 
 __global__ void substitute_CK_kernel(float *L_d, size_t L_h_nz, float *b_x_d, size_t n){
 	int tid = threadIdx.x;
@@ -26,11 +26,11 @@ __global__ void substitute_CK_kernel(float *L_d, size_t L_h_nz, float *b_x_d, si
 	extern __shared__ float b_x_s[];
 
 	int i, j;
-	int iter = (n << 1) / blockDim.x ;
-	if(((n << 1) % blockDim.x)!=0) iter += 1;
+	int iter = n / blockDim.x ;
+	if((n % blockDim.x)!=0) iter += 1;
 	for(i=0; i< iter; i++){
 		int base = i * blockDim.x;
-		if((base+tid) < 2 * n)
+		if((base+tid) < n)
 			b_x_s[base+tid] = b_x_d[base+tid];
 	}
 	__syncthreads();
@@ -69,7 +69,7 @@ __global__ void substitute_CK_kernel(float *L_d, size_t L_h_nz, float *b_x_d, si
 			row_p = tex1Dfetch(L_tex, i);
 				
 			// xi = bi / Aij
-			b_x_s[n+row_p] = b_x_s[row_p] / tex1Dfetch(L_tex, i+2);
+			b_x_s[row_p] /= tex1Dfetch(L_tex, i+2);
 
 			j = i-3;
 			if(j<0) break;
@@ -78,7 +78,7 @@ __global__ void substitute_CK_kernel(float *L_d, size_t L_h_nz, float *b_x_d, si
 				// bi = bi - Aij * xj
 				index_row = tex1Dfetch(L_tex, j);
 				index_col = tex1Dfetch(L_tex, j+1);
-				b_x_s[index_col] -= tex1Dfetch(L_tex, j+2) * b_x_s[n+index_row];
+				b_x_s[index_col] -= tex1Dfetch(L_tex, j+2) * b_x_s[index_row];
 				j -= 3;
 			}
 			i = j;
@@ -88,7 +88,7 @@ __global__ void substitute_CK_kernel(float *L_d, size_t L_h_nz, float *b_x_d, si
 	// after computing, copy back into global memory
 	for(i=0; i< iter; i++){
 		int base = i * blockDim.x;
-		if((base+tid) < 2 * n)
+		if((base+tid) < n)
 			//b_x_d[base+tid] = tex1Dfetch(L_tex, tid);
 			b_x_d[base+tid] = b_x_s[base+tid];
 	}
@@ -111,7 +111,7 @@ void substitute_CK_host(float *L_h, size_t L_h_nz, float *bp, float *xp, size_t 
 
 	dim3 dimGrid(1, 1);
 	dim3 dimBlock(256, 1, 1);
-	int sharedMemSize =sizeof(float) *2 *n;
+	int sharedMemSize =sizeof(float) *n;
 	clog<<"shared mem size: "<<sharedMemSize<<endl;
 	// perform for- and back-ward substitution for each block
 	// solution will be written from shared memory into global memory
@@ -119,7 +119,9 @@ void substitute_CK_host(float *L_h, size_t L_h_nz, float *bp, float *xp, size_t 
 	float cudaTime_compute;
 	CUT_SAFE_CALL(cutCreateTimer(&timer_compute));
 	CUT_SAFE_CALL(cutStartTimer(timer_compute));
+	
 	substitute_CK_kernel<<<dimGrid, dimBlock, sharedMemSize>>>(L_d, L_h_nz, b_x_d, n);
+	
 	cutilCheckMsg("Kernel execution failed.");
 	CUT_SAFE_CALL(cutStopTimer(timer_compute));
 	cudaTime_compute = cutGetTimerValue(timer_compute);
@@ -128,10 +130,8 @@ void substitute_CK_host(float *L_h, size_t L_h_nz, float *bp, float *xp, size_t 
 
 	// copy solution back from GPU into CPU
 	// where CPU will perform the find_diff and updaterhs()
-	//substitute_copy_back(x, b_x_d, b->nrow);
-	//substitute_CK_free(L_d, b_x_d);
 	//cutilSafeCall(cudaMemcpy(bp, b_x_d, sizeof(float)*n, cudaMemcpyDeviceToHost));
-	cutilSafeCall(cudaMemcpy(xp, &b_x_d[n], sizeof(float)*n, cudaMemcpyDeviceToHost));
+	cutilSafeCall(cudaMemcpy(xp, b_x_d, sizeof(float)*n, cudaMemcpyDeviceToHost));
 	cutilSafeCall(cudaUnbindTexture(L_tex));
 	cutilSafeCall(cudaFree(L_d));
 	cutilSafeCall(cudaFree(b_x_d));
@@ -157,22 +157,7 @@ void substitute_setup(float *L_h, size_t L_h_nz, float *&L_d, float *bp, float *
 	cutilSafeCall(cudaBindTexture(0, L_tex, L_d, channelDesc, count));
 		
 	// malloc b and x into a 1d array, copy from host to device
-	count = sizeof(float)* 2 * n;
+	count = sizeof(float) * n;
 	cutilSafeCall(cudaMalloc((void**)&b_x_d, count));	
-	size_t index = 0;
-	cutilSafeCall(cudaMemcpy(b_x_d, bp, sizeof(float)*n, cudaMemcpyHostToDevice));
-	index += n;
-	cutilSafeCall(cudaMemcpy(&b_x_d[index], xp, sizeof(float)*n, cudaMemcpyHostToDevice));
+	cutilSafeCall(cudaMemcpy(b_x_d, bp, count, cudaMemcpyHostToDevice));
 }
-
-/*
-void substitute_copy_back(cholmod_dense *x_h, double *b_x_d, size_t index){
-	size_t count = index; // both are nzmax
-	cudaMemcpy(&x_h->x, &b_x_d[count], count, cudaMemcpyDeviceToHost);
-}
-
-void substitute_CK_free(float *L_d, double *b_x_d){
-	cudaUnbindTexture(L_tex);
-	cudaFree(b_x_d);
-	cudaFree(L_d);
-}*/
